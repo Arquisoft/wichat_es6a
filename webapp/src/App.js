447efb17a70c02901b1b7f9126b6b2e96a5a9622
adrid64 <adridumitru01@gmail.com>
2025-02-27 11:01:30 +0100

    eliminado router de app.js

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,14 +1,19 @@
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import AddUser from './components/AddUser';
-import Login from './components/Login';
-import Home from './components/home';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AddUser from "./components/AddUser";
+import Login from "./components/Login";
+import Home from "./components/home";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 
 function App() {
   const [showLogin, setShowLogin] = useState(true);
@@ -23,10 +28,15 @@ function App() {
   };
 
   return (
-    <Router>
+    <>
       <CssBaseline />
       <Container component="main" maxWidth="xs">
-        <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
+        <Typography
+          component="h1"
+          variant="h5"
+          align="center"
+          sx={{ marginTop: 2 }}
+        >
           Welcome to the 2025 edition of the Software Architecture course
         </Typography>
 
@@ -35,26 +45,29 @@ function App() {
           <Route path="/Home" element={<Home />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-        
+
         {!showLogin && (
-  <Typography component="div" align="center" sx={{ marginTop: 2 }}>
-    <Link component="button" variant="body2" onClick={handleToggleView}>
-      Already have an account? Login here.
-    </Link>
-  </Typography>
-)}
+          <Typography component="div" align="center" sx={{ marginTop: 2 }}>
+            <Link component="button" variant="body2" onClick={handleToggleView}>
+              Already have an account? Login here.
+            </Link>
+          </Typography>
+        )}
 
-{showLogin && (
-  <Typography component="div" align="center" sx={{ marginTop: 2 }}>
-    <Link name="gotoregister" component="button" variant="body2" onClick={handleToggleView}>
-      Don't have an account? Register here.
-    </Link>
-  </Typography>
-)}
-
-        
+        {showLogin && (
+          <Typography component="div" align="center" sx={{ marginTop: 2 }}>
+            <Link
+              name="gotoregister"
+              component="button"
+              variant="body2"
+              onClick={handleToggleView}
+            >
+              Don't have an account? Register here.
+            </Link>
+          </Typography>
+        )}
       </Container>
-    </Router>
+    </>
   );
 }
 
