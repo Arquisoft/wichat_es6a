48b3f2129344a282ad7e0f1f17f4683ea3fd0761
emilio <uo257691@uniovi.es>
2025-03-06 15:54:28 +0100

    Chatbox now in a separate grid for better vertical resizing

--- webapp/src/components/GameWindow.js
+++ webapp/src/components/GameWindow.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
-import { Typography, Button } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import { Typography, Button, TextField } from "@mui/material";
 
 export function GameWindow() {
   const handleClick = () => {
@@ -9,146 +8,132 @@ export function GameWindow() {
   };
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      spacing={2}
-      sx={{
-        p: 5,
-        maxWidth: "100%",
-        maxHeight: "100%",
-        minWidth: 650,
-        margin: "auto",
-      }}
-    >
-      {/* Botones de Info y Exit */}
+    <Grid container sx={{ height: "100vh", width: "100vw" }}>
+      {/* Chatbox */}
       <Grid
         item
-        container
-        justifyContent="flex-end"
-        spacing={1}
-        sx={{ width: "100%" }}
-      >
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={handleClick}>
-            Hint
-          </Button>
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="error" onClick={handleClick}>
-            Exit
-          </Button>
-        </Grid>
-      </Grid>
-
-      <Grid
-        item
-        container
-        justifyContent="space-around"
-        sx={({ width: "100%" }, { mt: 3 })}
-      >
-        <Typography>Question 1/5</Typography>
-      </Grid>
-
-      {/* Imagen */}
-      <Grid
-        item
+        xs={3}
         sx={{
-          bgcolor: "whitesmoke",
-          width: "50%",
-          height: 450,
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          border: 1,
+          flexDirection: "column",
+          borderRight: "1px solid black",
+          p: 2,
+          height: "100vh",
         }}
       >
-        IMAGE
-      </Grid>
-
-      {/* Pregunta y Puntuación */}
-      <Grid
-        item
-        container
-        justifyContent="space-between"
-        sx={({ width: "100%" }, { mt: 3 })}
-      >
-        {/* Pregunta */}
-        <Typography sx={{ mt: 0 }}>
-          ¿Question number X----------------------------?
+        <Typography variant="h6" sx={{ mb: 2 }}>
+          Chat
         </Typography>
 
-        <Typography>Points: 600</Typography>
+        <Grid
+          sx={{
+            flexGrow: 1,
+            border: "1px solid black",
+            p: 1,
+            overflowY: "auto",
+          }}
+        >
+          {/* Mensajes del Chat */}
+          <Typography variant="body2">IA: How can I help you?</Typography>
+        </Grid>
+
+        {/* Campo de Entrada y Boton Enviar */}
+        <Grid container spacing={1} alignItems="center" sx={{ mt: 1 }}>
+          <Grid item xs={8}>
+            <TextField
+              variant="outlined"
+              size="small"
+              fullWidth
+              placeholder="Type a message..."
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              variant="contained"
+              size="small"
+              fullWidth
+              onClick={handleClick}
+            >
+              Send
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
 
-      {/* Contenedor de Chat y Respuestas en la Misma Fila */}
-      <Grid container sx={{ mt: 2, width: "100%" }} spacing={2}>
-        {/* Cuadro de Chat a la Izquierda */}
-        <Grid item xs={3}>
-          <Grid
-            container
-            direction="column"
-            justifyContent="space-between"
-            sx={{
-              border: "1px solid black",
-              width: "100%",
-              height: 150,
-              p: 1,
-            }}
-          >
-            {/* Mensajes del Chat (Simulación) */}
-            <Typography variant="body2" sx={{ mb: 1 }}>
-              IA: How can I help you?
-            </Typography>
-
-            {/* Campo de Entrada y Botón Enviar */}
-            <Grid container spacing={1} alignItems="center">
-              <Grid item xs={8}>
-                <TextField
-                  variant="outlined"
-                  size="small"
-                  fullWidth
-                  placeholder="Type a message..."
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <Button
-                  variant="contained"
-                  size="small"
-                  fullWidth
-                  onClick={handleClick}
-                >
-                  Send
-                </Button>
-              </Grid>
-            </Grid>
+      {/* Contenedor Principal */}
+      <Grid item xs={9} container direction="column" sx={{ p: 3 }}>
+        {/* Botones de Info y Exit */}
+        <Grid
+          item
+          container
+          justifyContent="flex-end"
+          spacing={1}
+          sx={{ mb: 2 }}
+        >
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={handleClick}>
+              Hint
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="error" onClick={handleClick}>
+              Exit
+            </Button>
           </Grid>
         </Grid>
 
-        {/* Respuestas a la Derecha */}
-        <Grid item xs={7}>
-          <Grid container spacing={1}>
-            <Grid item xs={6}>
-              <Button variant="outlined" fullWidth onClick={handleClick}>
-                Answer 1
-              </Button>
-            </Grid>
-            <Grid item xs={6}>
-              <Button variant="outlined" fullWidth onClick={handleClick}>
-                Answer 2
-              </Button>
-            </Grid>
-            <Grid item xs={6}>
-              <Button variant="outlined" fullWidth onClick={handleClick}>
-                Answer 3
-              </Button>
-            </Grid>
-            <Grid item xs={6}>
-              <Button variant="outlined" fullWidth onClick={handleClick}>
-                Answer 4
-              </Button>
-            </Grid>
+        {/* Pregunta */}
+        <Grid item sx={{ textAlign: "center", mb: 2 }}>
+          <Typography>Question 1/5</Typography>
+        </Grid>
+
+        {/* Imagen */}
+        <Grid
+          item
+          sx={{
+            bgcolor: "whitesmoke",
+            width: "50%",
+            height: 450,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: 1,
+            mx: "auto",
+            mb: 2,
+          }}
+        >
+          IMAGE
+        </Grid>
+
+        {/* Pregunta y Puntuacion */}
+        <Grid item container justifyContent="space-between" sx={{ mb: 2 }}>
+          <Typography>
+            ¿Question number X----------------------------?
+          </Typography>
+          <Typography>Points: 600</Typography>
+        </Grid>
+
+        {/* Respuestas */}
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+          <Grid item xs={6}>
+            <Button variant="outlined" fullWidth onClick={handleClick}>
+              Answer 1
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="outlined" fullWidth onClick={handleClick}>
+              Answer 2
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="outlined" fullWidth onClick={handleClick}>
+              Answer 3
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="outlined" fullWidth onClick={handleClick}>
+              Answer 4
+            </Button>
           </Grid>
         </Grid>
       </Grid>
