1423c51ddfbe51439b1e1f731f797d708ceefd90
Alex <alexrivada@gmail.com>
2025-03-20 18:08:02 +0100

    Creating a util connection file

---
+++ llmservice/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongodb-wichat_es6a:27017/wichatdb";
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Conectado a MongoDB"))
+.catch(err => console.error("Error en la conexión a MongoDB:", err));
+
+module.exports = mongoose;
