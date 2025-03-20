a377775a5820970bb0d22435b31df35d81a34941
Alex <alexrivada@gmail.com>
2025-03-20 18:09:48 +0100

    Unifying user and auth model

--- models/user-model.js
+++ models/user-model.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     username: {
       type: String,
       required: true,
+      unique: true
     },
     password: {
       type: String,
@@ -12,9 +13,9 @@ const userSchema = new mongoose.Schema({
     createdAt: {
       type: Date,
       default: Date.now, 
-    },
+    }
 });
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User
+module.exports = User;
