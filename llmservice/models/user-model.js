e951a9aa3dea37fc717d89248c1bd1a83ff08581
Alex <alexrivada@gmail.com>
2025-03-20 21:08:07 +0100

    User model adapted

---
+++ llmservice/models/user-model.js
@@ -0,0 +1,19 @@
+module.exports = (mongoose) => {
+  const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, 
+    }
+  });
+
+  return mongoose.model('User', userSchema);
+};
