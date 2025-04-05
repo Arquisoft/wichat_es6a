ce20a34f143932c76456cd6efac8ef89c3de1404
Alex <alexrivada@gmail.com>
2025-04-05 13:31:53 +0200

    Added questionSchema in stats-model

--- llmservice/models/stats-model.js
+++ llmservice/models/stats-model.js
@@ -1,10 +1,18 @@
 module.exports = (mongoose) => {
+  // Esquema de una pregunta
+  const questionSchema = new mongoose.Schema({
+    question: { type: String, required: true},
+    correctAnswer: { type: String, required: true},
+    incorrectAnswers: { type: [String], required: true }
+  });
+
   // Esquema de una partida
   const gameSchema = new mongoose.Schema({
     score: { type: Number, required: true },
     correctQuestions: { type: Number, required: true },
     recordedAt: { type: Date, default: Date.now },
-    gameId: { type: Number, required: true }
+    gameId: { type: Number, required: true },
+    questions: [questionSchema] // Lista de preguntas del juego
   });
 
   // Esquema del usuario con las partidas embebidas en el campo 'games'
