3ecb369521211bb7f70205d8f1366026cc983145
Iyansr97 <iyansr97@gmail.com>
2025-04-06 10:59:52 +0200

    Update user model

--- llmservice/models/user-model.js
+++ llmservice/models/user-model.js
@@ -3,20 +3,16 @@ module.exports = (mongoose) => {
     username: {
       type: String,
       required: true,
-      unique: true
+      unique: true,
     },
     password: {
       type: String,
       required: true,
     },
-    profilePic: {
-      type: Buffer,  // Almacenamos la imagen en formato binario
-      default: null, // Si no se proporciona una imagen, sera nula
-    },
     createdAt: {
       type: Date,
-      default: Date.now, 
-    }
+      default: Date.now,
+    },
   });
 
   return mongoose.model('User', userSchema);
